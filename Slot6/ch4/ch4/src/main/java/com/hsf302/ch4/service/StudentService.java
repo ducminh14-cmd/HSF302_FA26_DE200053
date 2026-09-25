d80db8cf73819d89b64fbeb73616c38fd8d26def
Duc Pham <mpham9495@gmail.com>
2026-09-25 14:52:40 +0700

    feat(builtin): sort and paginate students with findAll
    Refs: TODO 7

--- Slot6/ch4/ch4/src/main/java/com/hsf302/ch4/service/StudentService.java
+++ Slot6/ch4/ch4/src/main/java/com/hsf302/ch4/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.hsf302.ch4.service;
 
 import com.hsf302.ch4.pojo.Student;
+import org.springframework.data.domain.Page;
 
 import java.util.*;
 
@@ -8,4 +9,6 @@ public interface StudentService {
     long count();                                   // TODO 6
     Optional<Student> findById(Long id);
     // Các method được bổ sung dần từ TODO 6
+    List<Student> findAllOrderByGpaDesc();                              // TODO 7a
+    Page<Student> findPage(int pageIndex, int size, String sortField);  // TODO 7b
 }
