d713945672445188498cfab479a21ec77560e94b
Duc Pham <mpham9495@gmail.com>
2026-09-25 14:49:14 +0700

    feat(builtin): use count, findById and existsById
    Refs: TODO 6

--- Slot6/ch4/ch4/src/main/java/com/hsf302/ch4/service/DepartmentService.java
+++ Slot6/ch4/ch4/src/main/java/com/hsf302/ch4/service/DepartmentService.java
@@ -1,5 +1,7 @@
 package com.hsf302.ch4.service;
 
 public interface DepartmentService {
+    long count();                                   // TODO 6
+    boolean existsById(Long id);                    // TODO 6
     // Các method được bổ sung dần từ TODO 6
 }
--- Slot6/ch4/ch4/src/main/java/com/hsf302/ch4/service/StudentServiceImpl.java
+++ Slot6/ch4/ch4/src/main/java/com/hsf302/ch4/service/StudentServiceImpl.java
@@ -1,9 +1,12 @@
 package com.hsf302.ch4.service;
 
+import com.hsf302.ch4.pojo.Student;
 import com.hsf302.ch4.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +15,15 @@ public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
 
+    @Override
+    public long count() {
+        return studentRepository.count();
+    }
+
+    @Override
+    public Optional<Student> findById(Long id) {
+        return studentRepository.findById(id);
+    }
     // Các method được cài đặt dần từ TODO 6
 }
 
